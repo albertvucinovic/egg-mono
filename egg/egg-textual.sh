c0df7f3da4dfec5d77e7357189f46059b83a5fc4
Albert Vučinović <albert.vucinovic@gmail.com>
2025-11-12 22:11:53 +0100

    removing egg-textual

--- egg/egg-textual.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-CALLER_CWD="$(pwd)"
-
-cd $CALLER_CWD && source $SCRIPT_DIR/venv/bin/activate && set -a && source $SCRIPT_DIR/.env && set +a && python $SCRIPT_DIR/egg-textual.py
